65c9e468dfe22469139e5d5a5002c292dc06470c
bstarr322 <jointstarsoft@gmail.com>
2019-02-25 22:09:34 +0200

    change

--- app/controllers/app_proxy_controller.rb
+++ app/controllers/app_proxy_controller.rb
@@ -5,4 +5,22 @@ class AppProxyController < ApplicationController
     render layout: false, content_type: 'application/liquid'
   end
 
+  def update
+    shop = Shop.first
+    begin
+      shop.with_shopify_session do
+        customer = ShopifyAPI::Customer.find(params[:customer][:id])
+        customer.first_name = params[:customer][:first_name]
+        customer.last_name = params[:customer][:last_name]
+        customer.password = params[:customer][:password]
+        customer.password_confirmation = params[:customer][:password_confirmation]
+        customer.accepts_marketing = params[:customer][:accepts_marketing]
+        customer.save
+      end
+    rescue Exception => e
+      puts e
+      # render json: {success: false}, status: :ok
+    end
+    redirect_to "https://us.lintbells.com/account"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     # 	resources :reviews
     # GET /app_proxy/reviews will now be routed to
     # AppProxy::ReviewsController#index, for example
+    post '/update', to: 'update'
     get '/account', to: 'account#index'
   end
   get '/get_price_rule', to: 'api#get_price_rule'
