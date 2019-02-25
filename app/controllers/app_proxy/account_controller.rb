ce2f40b19b7e469a0f8924cf346de532b68c112c
bstarr322 <jointstarsoft@gmail.com>
2019-02-25 17:44:49 +0200

    proxy

---
+++ app/controllers/app_proxy/account_controller.rb
@@ -0,0 +1,7 @@
+class AccountController < ApplicationController
+  include ShopifyApp::AppProxyVerification
+  
+  def index
+    render layout: false, content_type: 'application/liquid'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
+
+  namespace :app_proxy do
+    root action: 'index'
+    # simple routes without a specified controller will go to AppProxyController
+    
+    # more complex routes will go to controllers in the AppProxy namespace
+    # 	resources :reviews
+    # GET /app_proxy/reviews will now be routed to
+    # AppProxy::ReviewsController#index, for example
+    get '/app_proxy/account'
+  end
   get '/get_price_rule', to: 'api#get_price_rule'
   post '/multipass', to: 'api#multipass'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
